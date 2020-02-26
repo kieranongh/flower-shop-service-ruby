9ac91b8e8bb5fc13a5260ddcc5f2d107095297f1
Kieran L <none>
2020-02-26 22:59:32 +1100

    Allow CORS
    - for this API, it's ok
      to allow all origins

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
 require 'sinatra'
+require 'sinatra/cross_origin'
 require 'json'
 
+set :bind, '0.0.0.0'
 set :port, 3001
+
+configure do
+  enable :cross_origin
+end
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
+
+options "*" do
+  response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  200
+end
 
 get '/bundles' do
   content_type :json
